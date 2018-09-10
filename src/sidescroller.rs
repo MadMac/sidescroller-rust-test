c263a3eea443e0137c531838ce4bd24c8df207ba
Markus Hällfors <markus@hallfors.com>
2018-09-10 20:10:10 +0300

    Read tileset image from .tmx file

--- src/sidescroller.rs
+++ src/sidescroller.rs
@@ -1,3 +1,5 @@
+extern crate tiled;
+
 use amethyst::assets::{AssetStorage, Loader};
 use amethyst::core::cgmath::{Matrix4, Vector3};
 use amethyst::core::transform::{GlobalTransform, Transform};
@@ -8,6 +10,12 @@ use amethyst::renderer::{
 	WithSpriteRender,
 };
 
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+use self::tiled::parse;
+
 pub struct Sidescroller;
 
 pub const CAMERA_WIDTH: f32 = 640.0;
@@ -15,7 +23,7 @@ pub const CAMERA_HEIGHT: f32 = 480.0;
 
 const SPRITESHEET_SIZE: (f32, f32) = (32.0, 32.0);
 
-use {Player};
+use Player;
 
 impl<'a, 'b> State<GameData<'a, 'b>> for Sidescroller {
 	fn on_start(&mut self, data: StateData<GameData>) {
@@ -32,6 +40,8 @@ impl<'a, 'b> State<GameData<'a, 'b>> for Sidescroller {
 				&texture_storage,
 			)
 		};
+
+		initialise_map(world);
 
 		world.register::<Player>();
 
@@ -62,11 +72,9 @@ fn initialise_camera(world: &mut World) {
 			CAMERA_WIDTH,
 			CAMERA_HEIGHT,
 			0.0,
-		)))
-		.with(GlobalTransform(
+		))).with(GlobalTransform(
 			Matrix4::from_translation(Vector3::new(0.0, 0.0, 1.0)).into(),
-		))
-		.build();
+		)).build();
 }
 
 fn initialise_player(world: &mut World, spritesheet: TextureHandle) {
@@ -78,7 +86,7 @@ fn initialise_player(world: &mut World, spritesheet: TextureHandle) {
 	};
 
 	let mut player_transform = Transform::default();
-	player_transform.translation = Vector3::new(16.0, 240.0, 0.0);	
+	player_transform.translation = Vector3::new(16.0, 240.0, 0.0);
 
 	world
 		.create_entity()
@@ -88,4 +96,31 @@ fn initialise_player(world: &mut World, spritesheet: TextureHandle) {
 		.with(GlobalTransform::default())
 		.with(player_transform)
 		.build();
+}
+
+fn initialise_map(world: &mut World) {
+
+	// TODO: Add to config
+	let path_to_maps = Path::new("./resources/maps/");
+
+	let mut map_file = File::open(path_to_maps.join("test1.tmx")).unwrap();
+	let reader = BufReader::new(map_file);
+	let map = parse(reader).unwrap();
+
+	let tileset_path = &map.get_tileset_by_gid(1).unwrap().images.get(0).unwrap().source;
+
+	let map_tileset = {
+		let loader = world.read_resource::<Loader>();
+		let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+		loader.load(
+			path_to_maps.join(tileset_path).to_str().unwrap(),
+			PngFormat,
+			Default::default(),
+			(),
+			&texture_storage,
+		)
+	};
+
+	println!("{:?}", map);
+	println!("{:?}", tileset_path);
 }
