bcb4dd2f42f3c79b64714126cd80975ef80b087b
Markus Hällfors <markus@hallfors.com>
2018-09-15 16:29:33 +0300

    Don't allow the player to get outside of the map in x axis

--- src/systems/player.rs
+++ src/systems/player.rs
@@ -1,8 +1,9 @@
 use amethyst::core::Transform;
-use amethyst::ecs::{Join, Read, System, WriteStorage};
+use amethyst::ecs::{Join, Read, System, WriteStorage, ReadExpect};
 use amethyst::input::InputHandler;
 
 use Player;
+use GameMap;
 
 const MOVEMENT_SCALE: f32 = 3.0;
 
@@ -11,10 +12,11 @@ impl<'s> System<'s> for PlayerSystem {
 	type SystemData = (
 		WriteStorage<'s, Transform>,
 		WriteStorage<'s, Player>,
+		ReadExpect<'s, GameMap>,
 		Read<'s, InputHandler<String, String>>,
 	);
 
-	fn run(&mut self, (mut transforms, mut players, input): Self::SystemData) {
+	fn run(&mut self, (mut transforms, mut players, game_map, input): Self::SystemData) {
 		for (player, transform) in (&mut players, &mut transforms).join() {
 			let movement = input.axis_value("running");
 
@@ -29,6 +31,15 @@ impl<'s> System<'s> for PlayerSystem {
 					player.v_velocity = -600.0;
 					transform.translation[1] += 1.0;
 				}
+			}
+
+			// Avoid out of bounds from map
+			if transform.translation[0] <= 1.0 {
+				transform.translation[0] = 1.0;
+			}
+
+			if transform.translation[0] >= ((&game_map.width-1)*32) as f32 {
+				transform.translation[0] = ((&game_map.width-1)*32) as f32;
 			}
 		}
 	}
